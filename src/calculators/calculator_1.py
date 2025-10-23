20bc738874ced6bb777467e5a741819218262d5e
xManoelx <manoeldr03@gmail.com>
2025-10-22 21:15:48 -0300

    Finalizando primeira calculadora

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -11,7 +11,7 @@ class Calculator1:
      Um número é dividido em 3 partes iguais.
 
      A primeira parte é dividida por 4 e seu resultado é somado a 7. Depois é elevado ao quadrado e multiplicado por 0.257
-     A segunda parte é elevada a potência de 2.212, dividida por 5 e somada a 1
+     A segunda parte é elevada a potência de 2.121, dividida por 5 e somada a 1
      A terceira parte se mantém no mesmo valor
 
      Por fim, os 3 resultados são somados e retornados.
@@ -27,7 +27,16 @@ class Calculator1:
         first_process_result = self.__first_process(splited_number)     # Primeiro processo de cálculo
         second_process_result = self.__second_process(splited_number)   # Segundo processo de cálculo
         third_process_result = self.__third_process(splited_number)     # Terceiro processo de cálculo
-        print(input_data)
+
+        calc_result = first_process_result + second_process_result + third_process_result
+        response = self.__format_response(calc_result)
+
+        print("--------------------------------------------------")
+        print("Input da Calculadora 1:", input_data)
+        print("Resultado da Calculadora 1:", calc_result)
+        print("--------------------------------------------------")
+        
+        return response
 
     # Validação do corpo da requisição
     def __validate_body(self, body: Dict) -> float:
@@ -44,10 +53,20 @@ class Calculator1:
 
     # Segundo processo de cálculo
     def __second_process(self, second_number: float) -> float:
-        result_secondPart = (second_number ** 2.212) / 5 + 1
+        result_secondPart = (second_number ** 2.121) / 5 + 1
         return result_secondPart
 
     # Terceiro processo de cálculo
     def __third_process(self, third_number: float) -> float:
         result_thirdPart = third_number
         return result_thirdPart
+    
+    # Formatação da resposta
+    def __format_response(self, calc_result: float) -> Dict:
+        return {
+            "data": {
+                "Calculator": 1,
+                "result": round(calc_result, 2)
+            }
+        }
+    
--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -8,6 +8,6 @@ calc_route_bp = Blueprint('calc_routes', __name__)
 @calc_route_bp.route('/calculator/1', methods=['POST'])
 def calculator_1():
     calc = Calculator1()
-    calc.calculate(request) # Chama o método calculate da classe Calculator1 
+    response = calc.calculate(request) # Chama o método calculate da classe Calculator1 
 
-    return jsonify({"success": True}), 200
+    return jsonify(response), 200
