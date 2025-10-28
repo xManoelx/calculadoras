2532c8e14e1d4577f8f2c2761838cf6ee8ce2b01
xManoelx <manoeldr03@gmail.com>
2025-10-28 20:22:13 -0300

    Implementando a rota da segunda calculadora

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
+from src.drivers.numpy_handler import NumpyHandler
 from src.calculators.calculator_1 import Calculator1
+from src.calculators.calculator_2 import Calculator2
 
 # Nomeia todas as rotas relacionadas a calculadoras
 calc_route_bp = Blueprint('calc_routes', __name__) 
@@ -9,5 +11,14 @@ calc_route_bp = Blueprint('calc_routes', __name__)
 def calculator_1():
     calc = Calculator1()
     response = calc.calculate(request) # Chama o método calculate da classe Calculator1 
+
+    return jsonify(response), 200
+
+# Rota para a calculadora 2
+@calc_route_bp.route('/calculator/2', methods=['POST'])
+def calculator_2():
+    numpy_handler = NumpyHandler()  # Instancia o manipulador de driver Numpy
+    calc = Calculator2(numpy_handler)
+    response = calc.calculate(request) # Chama o método calculate da classe Calculator2
 
     return jsonify(response), 200
