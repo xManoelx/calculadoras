a6b069be04a84c449bd9e2332c45911e2e9796c7
xManoelx <manoeldr03@gmail.com>
2025-11-01 19:35:33 -0300

    Finalizando logica da terceira calculadora

--- src/calculators/calculator_3.py
+++ src/calculators/calculator_3.py
@@ -1,6 +1,6 @@
 from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 from flask import request as FlaskRequest
-from typing import Dict
+from typing import Dict, List
 
 class Calculator3:
 
@@ -10,4 +10,46 @@ class Calculator3:
 
     # Função que realiza o cálculo específico da Calculator3
     def calculate(self, request: FlaskRequest) -> Dict: # type: ignore
-        pass
+        body = request.json()
+        input_data = self.__validate_body(body)
+
+        variance = self.__calculate_variance(input_data)
+        multiplication = self.__calculate_multiplication(input_data)
+        self.__verify_result(variance, multiplication)
+
+        formated_response = self.__format_response(multiplication)
+        return formated_response
+
+    # Funcao para validar o corpo da requisição
+    def __validate_body(self, body: Dict) -> list[float]:
+        if 'numbers' not in body:
+            raise Exception("Body mal formatado: 'numbers' ausente")
+        
+        input_data = body['numbers']
+        return input_data
+
+    # Funcao para calcular a variância dos números fornecidos
+    def __calculate_variance(self, numbers: List[float]) -> float:
+        variance = self.__driver_handler.variance(numbers)
+        return variance
+    
+    # Funcao para calcular a multiplicacao dos numeros fornecidos
+    def __calculate_multiplication(self, numbers: List[float]) -> float:
+        multiplication = 1 
+        for num in numbers: multiplication *= num
+        return multiplication
+    
+    # Funcao para verificar se o resultado do cálculo é válido
+    def __verify_result(self, variance: float, multiplication: float) -> None:
+        if variance < multiplication:
+            raise Exception("Resultado inválido: variância menor que multiplicação")
+        
+    # Formatação da resposta
+    def __format_response(self, variance: float) -> Dict:
+        return {
+            "data": {
+                "Calculator": 3,
+                "value": round(variance, 2),
+                "success": True
+            }
+        }
